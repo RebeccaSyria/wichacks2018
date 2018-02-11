b94e00eb43752e1ed58f717c980b3016eb2d0b0d
SkylerJaneClark <puffballinthedark@gmail.com>
2018-02-11 06:51:46 -0500

    ok this should not destroy everything we've worked for??? but can we really truly be sure?

--- Assets/Scripts/EnterBed.cs
+++ Assets/Scripts/EnterBed.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnterBed : MonoBehaviour {
-    
-    private void OnTriggerStay2D(Collider2D collision){
+	string[] hey = new string[1];
+
+	private void OnTriggerStay2D(Collider2D collision){
 	if(Input.GetKey(KeyCode.Space)){
-        GameObject tracker = GameObject.FindGameObjectWithTag("Track");
-        Debug.Log(tracker.GetComponent<Track>().day);
-        if( tracker.GetComponent<Track>().sleep == true)
-            {
-                GameObject faded = GameObject.Find("BlackFade");
-                faded.GetComponent<Animator>().enabled = true;
-                tracker.GetComponent<Track>().day++;
-                tracker.GetComponent<Track>().sleep = false;
-            }
-	}
+		hey[0] = "shit shit HI";
+		createTextBox();}
 }
-}
+
+	public GameObject createTextBox(){
+        GameObject obj = Instantiate(Resources.Load("TextDisplay") as GameObject);
+        obj.GetComponentInChildren<TextAdvance>().Init(hey);
+         return obj;
+	}}
--- Assets/Scripts/Walking.cs
+++ Assets/Scripts/Walking.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,56 +6,33 @@ public class Walking : MonoBehaviour {
 Rigidbody2D m_Rigidbody;
 SpriteRenderer m_SpriteRenderer;
 float m_Speed;
-bool canMoveLeft;
-bool canMoveRight;
     private bool initialize;
 	// Use this for initialization
 	void Start () {
 		m_Rigidbody = GetComponent<Rigidbody2D>();
 		m_SpriteRenderer = GetComponent<SpriteRenderer>();
 		m_Speed = 10.0f;
-    canMoveLeft = true;
-    canMoveRight = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		m_Speed = 20 * Input.GetAxis("Horizontal");
-    //Debug.Log(canMoveRight);
-    if( m_Speed < 0 && canMoveLeft){
-      m_Rigidbody.velocity = transform.right * m_Speed;
+		m_Rigidbody.velocity = transform.right * m_Speed;
+		if( m_Speed < 0){
 			m_SpriteRenderer.flipX = true;
-		}else if(m_Speed > 0 && canMoveRight){
-      m_Rigidbody.velocity = transform.right * m_Speed;
+		}else if(m_Speed > 0){
 			m_SpriteRenderer.flipX = false;
 		}
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log(collision.gameObject.name);
-        if (collision.gameObject.CompareTag("WallRight"))
+        if (collision.gameObject.CompareTag("Wall"))
         {
-          //  Debug.Log("Hit Wall");
-            canMoveRight = false;
+            Debug.Log("Hit Wall");
             m_Rigidbody.velocity = Vector2.zero;
-          //  Debug.Log(canMoveRight);
         }
-        if (collision.gameObject.CompareTag("WallLeft")){
-          canMoveLeft = false;
-          m_Rigidbody.velocity = Vector2.zero;
-        }
-
-    }
-    private void OnTriggerExit2D(Collider2D collision){
-      if(collision.gameObject.CompareTag("WallRight")){
-        canMoveRight = true;
-        //Debug.Log("exitted wall");
-      }
-      if(collision.gameObject.CompareTag("WallLeft")){
-        canMoveLeft = true;
-      }
-
     }
 
 }
